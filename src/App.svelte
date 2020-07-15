3f916e7aaafcf6ad50cd8991f5432040f9f8c46f
Peter Miller <peter.h.miller@gmail.com>
2020-07-15 10:23:47 -0400

    parity with traversy plus debounce

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,25 @@
 <script>
   import Header from './Header.svelte'
   import CharacterGrid from './CharacterGrid.svelte'
+  import Search from './Search.svelte'
   import { onMount } from 'svelte'
   let items = []
 
-  onMount(async () => {
-    const res = await fetch(`https://www.breakingbadapi.com/api/characters`)
+  onMount(async () => await searchFunction(''))
+
+  async function searchFunction(searchTerm) {
+    // empty items array to show spinner while doing query
+    items = []
+    const res = await fetch(
+      `https://www.breakingbadapi.com/api/characters?name=${searchTerm}`
+    )
     items = await res.json()
-  })
+  }
+
+  async function handleNewSearchTerm(event) {
+    let newSearchTerm = event.detail
+    await searchFunction(newSearchTerm)
+  }
 </script>
 
 <style>
@@ -20,5 +32,6 @@
 
 <div class="container">
   <Header />
+  <Search on:search={handleNewSearchTerm} />
   <CharacterGrid {items} />
 </div>
